40972b58a9262e830946a2e396348f37cb7cc5e9
yurirod <yurirobins@gmail.com>
2021-04-02 12:52:51 -0400

    Add project files.

---
+++ UsingSoapRequest.ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using UsingSoapRequest.ConsoleApp.CountryInfoService;
+
+namespace UsingSoapRequest.ConsoleApp
+{
+    class Program
+    {
+        private static IList<string> ListOfContinentsByName()
+        {
+            CultureInfo provider = CultureInfo.InvariantCulture;
+            //There are two endpoints in the configuration. Call one of them by its name
+            var client = new CountryInfoServiceSoapTypeClient("CountryInfoServiceSoap12");
+            var continents = client.ListOfContinentsByName(); // call of webmethod
+            IList<string> list = continents.Select(x=>x.sName).ToList();
+            return list;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List of continents:");
+            IList<string> list = ListOfContinentsByName();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
